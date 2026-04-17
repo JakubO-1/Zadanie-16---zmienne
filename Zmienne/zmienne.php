6719a2cf7a9893ce7f625177960b2e3181ceb254
JakubO-1 <jakuboliwa24@wp.pl>
2026-04-17 08:18:36 +0200

    Add files via upload

---
+++ Zmienne/zmienne.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 15;
+
+echo $a . "\n";
+echo -$a . "\n";
+echo ($a + 3) . "\n";
+echo ($a - 5) . "\n";
+echo ($a * 2) . "\n";
+echo ($a / 5) . "\n";
+echo ($a % 2) . "\n";
+echo ($a ** 3) . "\n";
+echo 'Wartość zmiennej $a wynosi: ' . $a . ' i już się nie zmieni.';
+
+?>
